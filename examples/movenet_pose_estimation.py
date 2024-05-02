6a1434bfffcb7b827c45a76680d670a42b4d97b5
Mathew <126032483+MathewAnderson6706@users.noreply.github.com>
2024-05-02 01:13:23 -0400

    Update movenet_pose_estimation.py

--- examples/movenet_pose_estimation.py
+++ examples/movenet_pose_estimation.py
@@ -1,153 +1,184 @@
-# Lint as: python3
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-r"""Example using PyCoral to estimate a single human pose with Edge TPU MoveNet.
-
-To run this code, you must attach an Edge TPU to the host and
-install the Edge TPU runtime (`libedgetpu.so`) and `tflite_runtime`. For
-device setup instructions, see coral.ai/docs/setup.
-
-For more details about MoveNet and its best practices, please see
-https://www.tensorflow.org/hub/tutorials/movenet
-
-Example usage:
-```
-bash examples/install_requirements.sh movenet_pose_estimation.py
-
-python3 examples/movenet_pose_estimation.py \
-  --model test_data/movenet_single_pose_lightning_ptq_edgetpu.tflite  \
-  --input test_data/squat.bmp
-```
-"""
-
-
-# The original code does pose detection of an image
-# This fork does is instead of an image, we use a webcam to do VideoCapture and do PoseDetection there
-# And instead of capturing all 17 points, we just capture 3 points(Wrist, Elbow, Shoulder)
-# Also added is a bicep counter where printed on the frame is a counter for how many curls the user does
-# Includes FPS and Right Wrist Location as well
 from PIL import Image
 from PIL import ImageDraw
 from pycoral.adapters import common
 from pycoral.utils.edgetpu import make_interpreter
+from sense_hat import SenseHat
 import cv2 as cv
 import numpy as np
 import time
-
+import math
+import speech_recognition as sr
+import google.generativeai as genai
+import os
+from multiprocessing import Process, Queue
+ 
 _NUM_KEYPOINTS = 17
-
+ 
+ 
 def calculate_angle(wrist, elbow, shoulder):
-    vc1 = wrist - elbow
-    vc2 = shoulder - elbow
-    dot_product = np.dot(vc1, vc2)
-    norm_product = np.linalg.norm(vc1) * np.linalg.norm(vc2)
+    vec1 = wrist - elbow
+    vec2 = shoulder - elbow
+    dot_product = np.dot(vec1, vec2)
+    norm_product = np.linalg.norm(vec1) * np.linalg.norm(vec2)
     angle_rad = np.arccos(dot_product / norm_product)
     angle_deg = np.degrees(angle_rad)
     return angle_deg
-
-
+ 
+def speech_recognition(queue):
+    #Voice and Ai Stuff
+    # Initialize the recognizer
+    response = " "
+    response_str = " "
+    recognizer = sr.Recognizer()
+    sense = SenseHat()
+    # Setting up my API Key
+    genai.configure(api_key='AIzaSyCSaKG2iGOTU_3SyaV1ujclVdlVHdkZyt0')
+ 
+    # Select the AI model
+    model = genai.GenerativeModel('gemini-pro')
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source, duration=1)
+        while True:
+          # Recognize speech using Google Web Speech API
+          audio_data = recognizer.listen(source)
+          try:
+              text = recognizer.recognize_google(audio_data)
+              prompt = "Identify the intent of the following text contained within curly braces. " \
+                      "The intent could be one of the following: A. Read Temperature B. Read Humidity " \
+                      "C. Reset Exercise Counter. Here few examples 1. if the text is {I want to start over}, " \
+                      "then return 'Reset Counter' as an output 2. if the text is {Restart} then return 'Reset Counter' " \
+                      "3. if the text is {How Hot is it} then return 'Read Temperature' 4. If the text is {What is the humidity} then return 'Read Humidity'. Here is the input text for your " \
+                      "response: {" + text + "}.  I want the response to be what I want, in just 1 line, nothing else."
+              response = model.generate_content(prompt)
+              response_str = str(response.text)
+              print(response_str)
+              queue.put(response_str)
+              #print(response.text)
+          except sr.UnknownValueError:
+              print("Google Web Speech API could not understand the audio")
+          except sr.RequestError as e:
+              print(f"Could not request results from Google Web Speech API; {e}")
+ 
 def main():
   # output='movenet_result.jpg'
   model = 'test_data/movenet_single_pose_lightning_ptq_edgetpu.tflite'
   interpreter = make_interpreter(model)
   interpreter.allocate_tensors()
-  
+  sense = SenseHat()
+  red = (255, 0, 0)
+  green = (0, 255, 0)
+ 
   cap = cv.VideoCapture(0)
   t, fps = 0, 0
   pTime = 0
   count = 0
+  count_knee = 0
+  knee = False
   curling = False
+ 
+ 
+  queue = Queue()
+  p = Process(target=speech_recognition, args=(queue,))
+  p.start()
+ 
   while True:
     ret, frame = cap.read()
     if not ret:
         break
-
+ 
+ 
     # Convert frame to PIL Image
     img = Image.fromarray(frame)
     resized_img = img.resize(common.input_size(interpreter), Image.ANTIALIAS)
     common.set_input(interpreter, resized_img)
-
+ 
     interpreter.invoke()
-
+ 
     pose = common.output_tensor(interpreter, 0).copy().reshape(_NUM_KEYPOINTS, 3)
-    
-    # Saving the specific values of the 3 we need
+ 
+    width, height = img.size
+    # Saving the specific values of the 6 we need
     r_wrist = pose[10]
+    l_wrist = pose[9]
     r_elbow = pose[8]
     r_shoulder = pose[6]
-    
+    r_ankle = pose[16]
+    l_knee = pose[13]
+ 
+    # Calculate the location of right anlke
+    r_ankle_y = int(pose[16][0] * height)
+ 
+    # Calculate the location of left knee
+    l_knee_y = int(pose[13][0] * height)
+ 
     # Calculating using the method of the 3 points
     angle_at_elbow = calculate_angle(r_wrist[:2], r_elbow[:2], r_shoulder[:2])
-    
+ 
     # Using the value to see if user is curling or not
     # To add to the count
     if angle_at_elbow < 30 and not curling:  
         count += 1
-        curling = True 
-    elif angle_at_elbow > 90:
+        count_str = str(count)
+        sense.show_message(count_str, text_colour = red)
+        curling = True
+    elif angle_at_elbow > 120:
         curling = False
-    
-    # Draw keypoints on the frame for only 3 points
-    draw = ImageDraw.Draw(img)
-    width, height = img.size
-    keypoint_indices = [6, 8, 10]
-    for i in keypoint_indices:
-        draw.ellipse(
-            xy=[
-                pose[i][1] * width - 2, pose[i][0] * height - 2,
-                pose[i][1] * width + 2, pose[i][0] * height + 2
-            ],
-            fill=(255, 0, 0))
-    
+ 
+    if r_ankle_y > l_knee_y and not knee:
+        count_knee += 1
+        count_knee_str = str(count_knee)
+        sense.show_message(count_knee_str, text_colour = green)
+        knee = True
+    elif r_ankle_y < l_knee_y:
+        knee = False
+  
+  
+    response_str = " "
+    if not queue.empty():
+        response_str = queue.get()
+
+    if response_str == "Read Temperature":
+        temp = sense.get_temperature()
+        temp_str = "{:.1f}C".format(temp)
+        sense.show_message(temp_str)
+    elif response_str == "Read Humidity":
+        humidity = sense.get_humidity()
+        humidity_str = "{:.1f}G/KG".format(humidity)
+        sense.show_message(humidity_str)
+    elif response_str == "Reset Exercise Counter":
+        count_knee = 0
+        count = 0
+        
+    response_str = " "
+ 
     # Convert back to OpenCV format and display the frame
     frame_with_keypoints = np.array(img)
-    
+ 
     # Calculate the location of the right wrist
     r_wrist_x = int(pose[10][1] * width)
     r_wrist_y = int(pose[10][0] * height)
-
-    # Display the location of the right wrist on the frame
-    cv.putText(frame_with_keypoints, f'Right Wrist: ({r_wrist_x}, {r_wrist_y})', (width - 400, 30), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
-    
-    # Display Right Bicep Curls on the frame
-    cv.putText(frame_with_keypoints, f'Right Bicep Curls: {count}', (width - 400, 60), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
-
-    
-    
-    # FPS
-    if t == 5:
-        cTime = time.time()
-        fps = 5 / (cTime - pTime)
-        pTime = cTime
-        t = 1
-    else:
-        t = t + 1
-    cv.putText(frame_with_keypoints, f'FPS: {int(fps)}', (20, 30), cv.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 3)
-    
-    
-    
-    
-    cv.imshow('Frame', frame_with_keypoints)
-    
-    
-    # Press 'q' quit
-    if cv.waitKey(1) & 0xFF == ord('q'):
+ 
+    # Calculate the location of the left wrist
+    l_wrist_x = int(pose[9][1] * width)
+    l_wrist_y = int(pose[9][0] * height)
+ 
+ 
+    distance = math.sqrt(math.pow(r_wrist_x - l_wrist_x, 2) + math.pow(l_wrist_x - l_wrist_y, 2))
+ 
+ 
+ 
+ 
+ 
+    # if both wrists come close together, than end
+    if distance < 65:
         break
-
+ 
+ 
+ 
   cap.release()
   cv.destroyAllWindows()
-
-
+  p.terminate()
+ 
+ 
 if __name__ == '__main__':
   main()
